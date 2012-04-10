1ecb8ec0219b9637d195318e070529503614dc1f
lehn <michael.lehn@uni-ulm.de>
2012-04-10 21:32:27 +0200

    Started with documentation.

--- examples/bspline.cc
+++ examples/bspline.cc
@@ -10,7 +10,7 @@ int
 main()
 {
     BSpline<double,Primal,R,CDF> phi(3);
-   
+
     for (double x=phi.support(0,0).l1; x<=phi.support(0,0).l2; x+=0.0125) {
         cout << x << " " << phi(x,0,0,0) << endl;
     }
