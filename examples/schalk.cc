14de72835f0b3f85896fe6c1b554bf5641e8bcde
Alexander Stippler <stip@iSchalk.local>
2012-04-10 21:32:16 +0200

    now with derivatives and producing gnuplot input.

--- examples/schalk.cc
+++ examples/schalk.cc
@@ -1,3 +1,4 @@
+#include <fstream>
 #include <iostream>
 
 #include <lawa/lawa.h>
@@ -9,12 +10,33 @@ using namespace std;
 int
 main()
 {
-    BSpline<double,Primal,R,CDF> phi(3);
+    const int j = 0;
+    const unsigned int d = 4;
+    const unsigned int deriv = 2;
+    BSpline<double,Primal,R,CDF> phi(d);
    
-    for (double x=phi.support(0,0).l1; x<=phi.support(0,0).l2; x+=0.0125) {
-        cout << x << " " << phi(x,0,0,0) << endl;
+    ofstream data("schalk.data");
+    int first = -phi.l2+1,
+        last  = -phi.l1-1;
+    for (int k=first; k<=last; ++k) { 
+        Support<double> supp = phi.support(j,k);
+        for (double x=supp.l1; x<=supp.l2; x+=0.0125) {
+            data << x << " " << phi(x,j,k,deriv) << endl;
+        }
+        data << endl << endl; 
     }
-    cout << endl;
+    data << endl;
+    data.close();
+
+    ofstream gps("schalk.gps");
+    gps << "set terminal png;" << endl;
+    gps << "set output 'N" << d << "_" << deriv << ".png'" << endl;
+    gps << "plot 'schalk.data' i 0 w l t''";
+    for (int i=first+1; i<=last; ++i) {
+        gps << ", 'schalk.data' i " << i-first << " w l t''";
+    }
+    gps << endl;
+    gps.close();
 
     return 0;
 }
